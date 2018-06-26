39097672524b53ef108ce21d23251663c865621a
bpeak <rlguswlgud@gmail.com>
2018-06-26 10:39:29 +0900

    heroku test

---
+++ app2.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 80
+
+app.listen(PORT, () => {
+    console.log('ok')
+})
+
+app.get('*', (req, res) => {
+    res.send('191')
+})
